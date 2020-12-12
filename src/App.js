876031054ce229496a21714723035ff81d439348
pras75299 <singh.prashantking@gmail.com>
2020-12-12 18:58:30 +0530

    create custom hook useLocalStorage to store data in local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import useLocalStorage from './useLocalStorage';
 
 function App() {
+  const [name, setName] = useLocalStorage('name', '');
   return (
     <div className="App">
       <h1>React Custom Hooks</h1>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Enter Name" />
     </div>
   );
 }
