ede492a74fa4466eea0615400d7681e3a9b9cc7c
pras75299 <singh.prashantking@gmail.com>
2020-12-13 09:25:12 +0530

    useInput custom hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './App.css';
-import useLocalStorage from './useLocalStorage';
+// import useLocalStorage from './useLocalStorage';
 import useUpdateLogger from './useUpdateLogger';
+import useInput from './useInput';
 
 function App() {
-  const [name, setName] = useLocalStorage('name', '');
-  useUpdateLogger(name);
+  // const [name, setName] = useLocalStorage('name', '');
+  const [firstName, bindFirstName, resetFirstName] = useInput('');
+  const [lastName, bindLastName, resetLastName] = useInput('');
+  useUpdateLogger(firstName, lastName);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert(`Hello ${firstName} ${lastName}`);
+    resetFirstName();
+    resetLastName();
+  }
   return (
-    <div className="App">
+    <div className="App"> 
+    
       <h1>React Custom Hooks</h1>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Enter Name" />
+      <form onSubmit={submitHandler}>
+        <div>
+          <label>First Name</label>
+          <input value={firstName} {...bindFirstName} type="text"/>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input value={lastName} {...bindLastName} type="text"/>
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
